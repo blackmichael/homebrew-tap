a4faf0c3444d7a4dcea1f3c8ea9bd4b2782404dd
Michael Black <michaelpblack16@gmail.com>
2026-04-25 11:38:05 -0500

    Update tuisky to 1.0.0 (#2)

---
+++ Formula/tuisky.rb
@@ -0,0 +1,24 @@
+class Tuisky < Formula
+  desc "Terminal Bluesky client"
+  homepage "https://github.com/blackmichael/tuisky"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/blackmichael/tuisky/releases/download/v1.0.0/tuisky-1.0.0-darwin-arm64.tar.gz"
+    sha256 "1c6d5da7f7d6366918e218dce87d7095e6f039c1ce951ee0b24e9219c7f9d85c"
+  else
+    url "https://github.com/blackmichael/tuisky/releases/download/v1.0.0/tuisky-1.0.0-darwin-x64.tar.gz"
+    sha256 "0f458c17ca13d4c3a81b3be3370cc0ac221eac68fd25989537d746e62f21b0ee"
+  end
+
+  version "1.0.0"
+  license "MIT"
+
+  def install
+    odie "tuisky is only supported on macOS" unless OS.mac?
+    bin.install "tuisky"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/tuisky --version")
+  end
+end
